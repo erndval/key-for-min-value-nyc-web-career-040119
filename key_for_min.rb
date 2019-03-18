0db94b7aa68fe710197ae3f83ac06f94f7b0b681
Ernie Val <erndval@gmail.com>
2019-03-18 16:09:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |item, count|
     if count < lowest
       lowest = count
-  lowest_key = item
+      lowest_key = item
 end
 if lowest_key == ""
   return NIL
